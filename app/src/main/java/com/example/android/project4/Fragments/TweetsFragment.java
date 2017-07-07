4c8659c0637cf5b27eb5ac96a1daf64ccf058148
Aman Dhapola <amandhapola@gmail.com>
2017-07-08 04:19:33 +0530

    changes something in fragment

--- app/src/main/java/com/example/android/project4/Fragments/TweetsFragment.java
+++ app/src/main/java/com/example/android/project4/Fragments/TweetsFragment.java
@@ -60,7 +60,8 @@ public class TweetsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tweets, container, false);
+        View v =  inflater.inflate(R.layout.fragment_tweets, container, false);
+        return v;
     }
 
 }
